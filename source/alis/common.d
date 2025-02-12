67211341bfc3760545b70f74e31db670e42c7746
Nafees Hassan <nafeeshassan16@gmail.com>
2025-02-13 00:23:15 +0500

    Added Resolution Node struct

--- source/alis/common.d
+++ source/alis/common.d
@@ -1,9 +1,90 @@
 module alis.common;
 
-/// A resolution chain node
-public struct ResNode{
-	/// next, if any
-	ResNode* next;
+/// A resolution chain node.
+public struct Resolution{
+private:
+	Resolution* _leaf;
+	Resolution* _next;
+	Resolution* _prev;
+
+public:
+	/// previous resolution, if any
+	@property Resolution* prev() pure {
+		return _prev;
+	}
+	/// next resolution, if any
+	@property Resolution* next() pure {
+		return _next;
+	}
+
+	/// Returns: most resolved Resolution. will be `this` if `next is null`
+	@property Resolution* leaf() pure {
+		if (_leaf is null){
+			if (_next is null)
+				return _leaf = &this;
+			return _leaf = next.leaf;
+		}
+		if (_leaf.next){
+			_leaf._leaf = null;
+			return _leaf = _leaf.leaf;
+		}
+		return _leaf;
+	}
+	/// pushes a new resolution at end of this list
+	void push(Resolution* r) pure {
+		leaf._next = r;
+	}
+
+	/// Types of what is being resolved
+	enum TypeS{
+		Ident, /// identifier resolution
+		Alias, /// alias resolution
+		OpBinEval, /// binary operator evaluation resolution
+		OpPreEval, /// prefix operator evaluation resolution
+		OpPostEval, /// postfix operator evaluation resolution
+		Intrinsic, /// intrinsic resolution
+		IntrinsicCall, /// intrinsic call resolution
+		Call, /// call resolution
+		OpDot, /// OpDot resolution
+	}
+
+	/// Types of what it resolved to
+	enum TypeT{
+		Alias, /// resolves to Alias
+		DataType, /// resolves to a Data Type
+		Fn, /// resolves to a function
+		Template, /// resolves to a template
+		Mixin, /// resolves to a mixin template
+		Var, /// resolves to a var
+		TParam, /// resolves to a template parameter.
+						/// TODO: maybe resolve it completely here?
+		FParam, /// resolves to a funtion parameter
+						/// TODO: maybe resolve it completely here?
+		EnumConst, /// resolves to an enum const definition. Regular enum->DataType
+		EnumMemeber, /// resolves to enum member
+		Import, /// resolves to an imported module's aliased name
+	}
+
+	/// what is being resovled
+	TypeS typeS;
+	/// result from resolution
+	ASymbol sym; // TODO: store ptr?
+	union{
+		/// subject symbol name, for `Ident`, `Intrinsic`, or `IntrinsicCall`
+		string nameS;
+		/// subject symbol for `Alias`, or `Call`
+		ASymbol* symS;
+		struct{
+			string op; /// operator, for `OpBinEval`, `OpPreEval`, or `OpPostEval`
+			union{
+				ASymbol* operand; /// operand, for `OpPreEval`, or `OpPostEval`
+				struct{
+					ASymbol* operandLhs; /// LHS operand, for `OpBinEval`
+					ASymbol* operandRhs; /// RHS operand, for `OpBinEval`
+				}
+			}
+		}
+	}
 }
 
 /// Alis Data Type
@@ -68,10 +149,10 @@ public struct AUnion{
 	}
 }
 
-/// a symbol table entry
+/// a symbol
 public struct ASymbol{
 public:
-	/// symbol name
+	/// symbol name (in its own local scope)
 	string name;
 	// TODO implement ASymbol
 }
