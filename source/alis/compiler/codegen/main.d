009567188477039008f027e0e76d8644d13383c2
Muhammad Talha <shmdtalha@gmail.com>
2025-03-10 16:04:08 +0500

    Now able to push literals

--- source/alis/compiler/codegen/main.d
+++ source/alis/compiler/codegen/main.d
@@ -11,6 +11,8 @@ import std.array;
 import std.conv;
 import std.format;
 
+const string testFolder = "tests/codegen/";
+
 class BytecodeGenerator {
 	private string[][] bytecodeInstructions;
 	private int labelCounter = 0;
@@ -29,13 +31,14 @@ class BytecodeGenerator {
 		bytecodeInstructions ~= [instruction] ~ params;
 	}
 
+
+
 	private string generateLabel(string prefix = "label") {
 		return format!"%s_%d"(prefix, labelCounter++);
 	}
 
 	private void generateStatementBytecode(RStatement statement) {
-		writeln("Statement type: ", typeid(statement).toString, "\n");
-		
+	
 		if (auto block = cast(RBlock)statement) {
 			generateBlockBytecode(block);
 		} else if (auto returnStmt = cast(RReturn)statement) {
@@ -51,14 +54,14 @@ class BytecodeGenerator {
 		} else if (auto switchStmt = cast(RSwitch)statement) {
 			generateSwitchBytecode(switchStmt);
 		} else if (auto expr = cast(RExpr)statement) {
-			generateExpressionBytecode(expr);
+			//generateExpressionBytecode(expr);
 		} else {
 			throw new Exception("Unsupported statement type: " ~ typeid(statement).toString);
 		}
 	}
 
 	private void generateBlockBytecode(RBlock block) {
-		writeln("Mock: generateBlockBytecode called: ") ;
+		writeln("Mock: generateBlockBytecode called") ;
 		foreach (statement ; block.statements){
 			generateStatementBytecode(statement);
 			
@@ -66,11 +69,42 @@ class BytecodeGenerator {
 	}
 
 	private void generateReturnBytecode(RReturn returnStmt) {
-		writeln("Mock: generateReturnBytecode called: ");
+		writeln("Mock: generateReturnBytecode called");
 	}
 
+
 	private void generateIfBytecode(RIf ifStmt) {
-		writeln("Mock: generateIfBytecode called: ");
+		writeln("Calling GenerateIfBytecode");
+
+		string endLabel = generateLabel("if_end");
+		string elseLabel = generateLabel("if_else");
+		
+		// Generate condition expression bytecode
+		//generateExpressionBytecode(ifStmt.condition);
+		
+		// Jump to else branch if condition is false
+		if (ifStmt.onFalse) {
+			addInstruction("jmpC", [elseLabel]);
+			
+			// Generate true branch code
+			generateStatementBytecode(ifStmt.onTrue);
+			
+			// Skip over else branch
+			addInstruction("jmp", [endLabel]);
+			
+			// Else branch
+			addInstruction(elseLabel ~ ":");
+			generateStatementBytecode(ifStmt.onFalse);
+		} else {
+			// If there's no else branch, just skip the true branch if condition is false
+			addInstruction("jmpC", [endLabel]);
+			
+			// Generate true branch code
+			generateStatementBytecode(ifStmt.onTrue);
+		}
+		
+		// End of if statement
+		addInstruction(endLabel ~ ":");
 	}
 
 	private void generateForBytecode(RFor forStmt) {
@@ -94,13 +128,7 @@ class BytecodeGenerator {
 		writeln("Mock: generateSwitchBytecode called: ");
 	}
 
-	private void generateExpressionBytecode(RExpr expr) {
-		writeln("Mock: generateExpressionBytecode called: ");
-	}
 
-	private void generateLiteralBytecode(RLiteralExpr literalExpr) {
-		// addInstruction("push", [convertLiteralToType(literalExpr), convertLiteralToValue(literalExpr)]);
-	}
 
 	// Utility methods to convert literals
 	private string convertLiteralToType(RLiteralExpr literalExpr) {
@@ -122,12 +150,136 @@ class BytecodeGenerator {
 		}
 	}
 
+	// 
+	// private void generateExpressionBytecode(RExpr expr) {
+	// 	if (auto literal = cast(RLiteralExpr)expr) {
+	// 		generateLiteralBytecode(literal);
+
+
+	// 	} else if (auto identExpr = cast(RIdentExpr)expr) {
+	// 		// Handle identifier
+	// 		int offset = 0; // Would need to determine actual offset from symbol table
+	// 		string typeStr = identExpr.ident.toString;
+			
+	// 		if (typeStr.startsWith("int")) {
+	// 			int size = to!int(typeStr[3..$]);
+	// 			addInstruction("get", [to!string(size/8), to!string(offset)]);
+	// 		} else if (typeStr.startsWith("float")) {
+	// 			int size = to!int(typeStr[5..$]);
+	// 			addInstruction("get", [to!string(size/8), to!string(offset)]);
+	// 		}
+	// 	} else if (auto assignExpr = cast(RAssignExpr)expr) {
+	// 		// For assignment, generate the right side first (which puts value on stack),
+	// 		// then store it to the location of the left side
+	// 		generateExpressionBytecode(assignExpr.rhs);
+			
+	// 		// Determine the left side location
+	// 		if (auto identExpr = cast(RIdentExpr)assignExpr.lhs) {
+	// 			int offset = 0; // Would need actual offset
+	// 			string typeStr = identExpr.ident.toString;
+				
+	// 			if (typeStr.startsWith("int")) {
+	// 				int size = to!int(typeStr[3..$]);
+	// 				addInstruction("putI" ~ to!string(size/8), [to!string(offset)]);
+	// 			} else if (typeStr.startsWith("float")) {
+	// 				int size = to!int(typeStr[5..$]);
+	// 				addInstruction("putF" ~ to!string(size/8), [to!string(offset)]);
+	// 			}
+	// 		}
+	// 	} else if (auto fnCallExpr = cast(RFnCallExpr)expr) {
+	// 		// Push parameters in reverse order
+	// 		foreach_reverse (param; fnCallExpr.params) {
+	// 			generateExpressionBytecode(param);
+	// 		}
+			
+	// 		// Generate callee expression
+	// 		if (auto identExpr = cast(RIdentExpr)fnCallExpr.callee) {
+	// 			string funcName = identExpr.ident.toString;
+	// 			addInstruction("call", ["@" ~ funcName]);
+	// 		} else {
+	// 			// More complex function call (function pointer, etc.)
+	// 			generateExpressionBytecode(fnCallExpr.callee);
+	// 			addInstruction("call", ["R"]);  // Call through register
+	// 		}
+	// 	} else if (auto commaExpr = cast(RCommaExpr)expr) {
+	// 		// Evaluate all expressions, but only the last one's value is kept
+	// 		foreach (subExpr; commaExpr.exprs) {
+	// 			generateExpressionBytecode(subExpr);
+				
+	// 			// Pop intermediate results except the last one
+	// 			if (subExpr != commaExpr.exprs[$-1]) {
+	// 				addInstruction("popN", ["1"]);
+	// 			}
+	// 		}
+	// 	} else if (auto refExpr = cast(RRefExpr)expr) {
+	// 		// Get reference to value
+	// 		if (auto identExpr = cast(RIdentExpr)refExpr.val) {
+	// 			int offset = 0; // Would need actual offset
+	// 			addInstruction("pshO");
+	// 			addInstruction("pshI4", [to!string(offset)]);
+	// 			addInstruction("addI4");
+	// 		} else {
+	// 			// More complex reference
+	// 			generateExpressionBytecode(refExpr.val);
+	// 			addInstruction("getR");
+	// 		}
+	// 	} else if (auto derefExpr = cast(RDerefExpr)expr) {
+	// 		// Dereference a pointer
+	// 		generateExpressionBytecode(derefExpr.val);
+	// 		addInstruction("get", ["4", "0"]); // Assuming 4-byte pointer dereferencing
+	// 	}
+	// 	// Add more expression types as needed
+	// }
+
+	// Generates Bytecode of Literals
+	private void generateLiteralBytecode(RLiteralExpr literalExpr) {
+		ADataType.Type type = (*literalExpr.type).type;
+		string instruction;
+		string[] params;
+		
+		if (type == ADataType.Type.IntX) {
+			// Convert the byte array to the appropriate IntX type
+			int size = literalExpr.type.x;
+			instruction = "pshI" ~ to!string(size);
+			
+			
+			if (size == 1) {
+				byte value = as!byte(literalExpr.value);
+				params = [to!string(value)];
+			} else if (size == 2) {
+				short value = as!short(literalExpr.value);
+				params = [to!string(value)];
+			} else if (size == 4) {
+				int value = as!int(literalExpr.value);
+				params = [to!string(value)];
+			} else if (size == 8) {
+				long value = as!long(literalExpr.value);
+				params = [to!string(value)];
+			}
+		} else if (type == ADataType.Type.FloatX) {
+			// Convert the byte array to the appropriate FloatX type
+
+			int size = literalExpr.type.x;
+			instruction = "pshF" ~ to!string(size);
+			
+			if (size == 4) {
+				float value = as!float(literalExpr.value);
+				params = [to!string(value)];
+			} else if (size == 8) {
+				double value = as!double(literalExpr.value);
+				params = [to!string(value)];
+			}
+		}
+		
+		addInstruction(instruction, params);
+	}
+
 
 }
 
 
 void printBytecode(string[][] bytecodeInstructions) {
-	writeln("Generated Bytecode:");
+	writefln("Generated %d Bytecode Instructions:", bytecodeInstructions.length);
 	writeln("------------------");
 	foreach (size_t lineNum, instruction; bytecodeInstructions) {
 		if (instruction.length == 0) continue;
@@ -142,7 +294,7 @@ void printBytecode(string[][] bytecodeInstructions) {
 		}
 	}
 	writeln("------------------");
-	writefln("Total Instructions: %d", bytecodeInstructions.length);
+	
 }
 
 void printBytecodeToFile(string filename, string[][] bytecodeInstructions) {
@@ -186,7 +338,7 @@ private ADataType* getTypeFromString(string typeStr) {
 
 // Tesing conversion of literals to bytes and then back to values 
 unittest{
-	string testFolder = "tests/codegen/";
+	
 	auto generator = new BytecodeGenerator;
 
 	// Integer literal
@@ -198,13 +350,11 @@ unittest{
 	auto floatLiteral = new RLiteralExpr;
 	floatLiteral.type = getTypeFromString("float32");
 	floatLiteral.value = asBytes!float(1.0f);
-	writeln(floatLiteral.value);
 
 	// Double literal
 	auto doubleLiteral = new RLiteralExpr;
 	doubleLiteral.type = getTypeFromString("float64");
 	doubleLiteral.value = asBytes!double(3.14159);
-	writeln(doubleLiteral.value);
 
 	// Short literal
 	auto shortLiteral = new RLiteralExpr;
@@ -264,6 +414,40 @@ unittest{
 	printBytecodeToFile(testFolder ~  "complex_code.txt", complexBytecode);
 
 }
+
+// Testing literal expressions
+unittest {
+
+	auto generator = new BytecodeGenerator;
+
+	// Test for 4-byte int literal
+	auto intLiteral = new RLiteralExpr;
+	intLiteral.type = new ADataType(false, ADataType.Type.IntX, 4);
+	intLiteral.value = asBytes!int(42);
+	generator.generateLiteralBytecode(intLiteral);
+
+	// Test for 8-byte long literal
+	auto longLiteral = new RLiteralExpr;
+	longLiteral.type = new ADataType(false, ADataType.Type.IntX, 8);
+	longLiteral.value = asBytes!long(123456789L);	
+	generator.generateLiteralBytecode(longLiteral);
+
+	// Test for 32-bit float literal
+	auto floatLiteral = new RLiteralExpr;
+	floatLiteral.type = new ADataType(false, ADataType.Type.FloatX, 4);
+	floatLiteral.value = asBytes!float(3.14f);	
+	generator.generateLiteralBytecode(floatLiteral);
+
+	// // Test for 64-bit double literal
+	auto doubleLiteral = new RLiteralExpr;
+	doubleLiteral.type = new ADataType(false, ADataType.Type.FloatX, 8);
+	doubleLiteral.value = asBytes!double(2.717);	
+	generator.generateLiteralBytecode(doubleLiteral);
+
+	printBytecodeToFile(testFolder ~  "literal_code.txt", generator.bytecodeInstructions);
+	printBytecode(generator.bytecodeInstructions);
+
+} 
 
 
 version (codegen) {
