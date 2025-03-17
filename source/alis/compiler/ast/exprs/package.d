3c7f06fd837b3489955411a00b23e50ad7aaa579
Nafees Hassan <nafeeshassan16@gmail.com>
2025-03-18 01:46:28 +0500

    Merged that fat AST.
    splitting it was a mistake

--- source/alis/compiler/ast/exprs/package.d
+++
@@ -1,9 +0,0 @@
-module alis.compiler.ast.exprs;
-
-public import
-			 alis.compiler.ast.exprs.base,
-			 alis.compiler.ast.exprs.anondef,
-			 alis.compiler.ast.exprs.idents,
-			 alis.compiler.ast.exprs.intrinsics,
-			 alis.compiler.ast.exprs.literals,
-			 alis.compiler.ast.exprs.operators;
