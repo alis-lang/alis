7d9586d1f598d6a4457527e2577323b5b368ebca
Nafees Hassan <nafeeshassan16@gmail.com>
2025-03-29 16:52:25 +0500

    Implemented SmErr

--- source/alis/compiler/semantic/error.d
+++ source/alis/compiler/semantic/error.d
@@ -5,15 +5,39 @@ module alis.compiler.semantic.error;
 
 import alis.compiler.common;
 
+import std.format;
+
 /// Value or SmErr
 alias SmErrVal(T) = ErrVal!(T, SmErr);
 /// Value or SmErrs
 alias SmErrsVal(T) = ErrVal!(T, SmErr[]);
 
 /// Alis Semantic Error
-struct SmErr{
+public struct SmErr{
 	/// Possible Error Types
 	enum Type{
 		IdentReuse, /// Same identifier used across definitions
 	}
+	/// where error happen
+	Location pos;
+	/// error message
+	string msg;
+	/// type
+	Type type;
+	/// string representation
+	string toString() const pure {
+		return format!"%s: %s"(pos, msg);
+	}
+	@disable this();
+	this (Location pos, string msg, Type type){
+		this.pos = pos;
+		this.msg = msg;
+		this.type = type;
+	}
+}
+
+/// Identifier re-use error
+package SmErr errIdentReuse(Location pos, string ident){
+	return SmErr(pos, format!"Identifier `%s` conflicts"(ident),
+			SmErr.Type.IdentReuse);
 }
