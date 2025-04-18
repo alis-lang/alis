788003ede8a2634fe39e1b8914e4aa90ab085e2a
Nafees Hassan <nafeeshassan16@gmail.com>
2025-04-19 02:16:55 +0500

    bye bye `analyse`

--- source/alis/compiler/semantic/package.d
+++ source/alis/compiler/semantic/package.d
@@ -11,12 +11,3 @@ import alis.common,
 			 alis.compiler.rst,
 			 alis.compiler.ast,
 			 alis.compiler.semantic.symbols;
-
-/// Does semantic analysis
-package CmpMErrVal!(Tuple!(RModule, AModule)) analyse(Module moduleNode){
-	CmpMErrVal!(Tuple!(RModule, AModule)) ret;
-	STState symResState;
-	STIter.iterate(moduleNode, symResState);
-	// TODO: extract result from symResState into ret
-	return ret;
-}
