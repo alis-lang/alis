ac09fc1973315b7f83333047ab11ca3ca982e0bf
Nafees Hassan <nafeeshassan16@gmail.com>
2025-04-24 08:44:48 +0500

    structs pretending to be functions and more madness like that

--- source/alis/compiler/semantic/symbols.d
+++ source/alis/compiler/semantic/symbols.d
@@ -7,6 +7,7 @@ import std.algorithm,
 			 std.typecons,
 			 std.range,
 			 std.traits,
+			 std.format,
 			 std.meta;
 
 import utils.ds;
@@ -18,7 +19,8 @@ import alis.common,
 			 alis.compiler.semantic.common,
 			 alis.compiler.semantic.error,
 			 alis.compiler.ast,
-			 alis.compiler.ast.iter;
+			 alis.compiler.ast.iter,
+			 alis.compiler.rst;
 
 import meta;
 
@@ -35,7 +37,7 @@ package SmErrsVal!(STab!ASymbol) sTabBuild(Module mod){
 /// Returns: symbol initial symbol table
 private SmErrsVal!(STab!DefNode) sTab0Build(Module mod){
 	St0 st0;
-	It!(Lv.Mod).exec(mod, st0);
+	//It!(Lv.Mod).exec(mod, st0); // TODO: FIX THIS!
 	if (st0.errs.length)
 		return SmErrsVal!(STab!DefNode)(st0.errs);
 	return SmErrsVal!(STab!DefNode)(st0.st);
@@ -50,10 +52,67 @@ private SmErrsVal!(STab!ASymbol) sTab1Build(STab!DefNode stab){
 			state.st = ret;
 			state.st0 = stab;
 			state.visId = visId;
-			It!(Lv.Defs).exec(def, state);
+			//It!(Lv.Defs).exec(def, state);
 		}
 	}
 	return SmErrsVal!(STab!ASymbol)(ret);
+}
+
+/// Gets imports at any node in the AST (yes I know it's a struct, it has an
+/// opCall override)
+///
+/// Returns: Imports
+struct importsOf{
+	@disable this();
+private:
+	alias It = ASTIter!(globDefIter, localDefIter, importIter);
+static:
+	struct St{
+		SmErr[] errs;
+		Imports imp;
+	}
+
+	@ItFn void globDefIter(GlobDef n, ref St state){
+		It.exec(n.def, state);
+	}
+	@ItFn void localDefIter(DefStatement n, ref St state){
+		It.exec(n.def, state);
+	}
+	@ItFn void importIter(Import n, ref St state){
+		if (n.name == "_"){
+			if (n.name in state.imp.importsN){
+				state.errs ~= errIdentReuse(n.pos, n.name);
+				return;
+			}
+			state.imp.importsN[n.name] = n.moduleIdent.mod;
+			return;
+		}
+		state.imp.imports ~= n.moduleIdent.mod;
+	}
+
+public static:
+	SmErrsVal!Imports opCall(ASTNode node){
+		St st;
+		It.exec(node, st);
+		if (st.errs)
+			return SmErrsVal!Imports(st.errs);
+		return SmErrsVal!Imports(st.imp);
+	}
+}
+
+/// Builds AModule from Module
+/// Returns: AModule
+struct aModOf{
+	@disable this();
+private:
+
+static:
+
+public static:
+	SmErrsVal!AModule opCall(Module mod){
+		AModule ret;
+		return SmErrsVal!AModule(ret);
+	}
 }
 
 /// iteration levels
@@ -62,7 +121,7 @@ private enum Lv : size_t{
 	Defs, /// converting module level definitions to ASymbols
 }
 
-private alias It(Lv l) = ItL!(mixin(__MODULE__), l);
+//private alias It(Lv l) = ItL!(mixin(__MODULE__), l);
 
 /// Iteration state for L.Mod
 private struct St0{
@@ -76,7 +135,7 @@ private struct St0{
 	SmErr[] errs;
 }
 
-private @ITL(Lv.Mod) @ItFn {
+/*private @ITL(Lv.Mod) @ItFn {
 	void modIter(Module node, ref St0 state){
 		state.st = new STab!DefNode;
 		state.modId = node.ident.IdentU;
@@ -107,43 +166,55 @@ private @ITL(Lv.Mod) @ItFn {
 		// kinda sad tbh
 		def.unsupported;
 	}
-}
+}*/
 
 /// Iteration state for Lv.Defs
 private struct St1{
 	STab!DefNode st0;
 	STab!ASymbol st;
+	RFn[] fns;
+	void[0][DefNode] visited;
 	IdentU visId;
+	IdentU[] ctx;
 }
 
 private @ITL(Lv.Defs) @ItFn {
 	void mixinInitDefConv(MixinInitDef node, ref St1){
 		node.unsupported;
 	}
-	void importConv(Import node, ref St1 state){
-		STab!ASymbol modSTab = node.moduleIdent.modSTab;
-		// TODO: merge imported stab
-	}
-	void fnConv(FnDef node, ref St1 state){}
-	void enumConstConv(EnumConstDef node, ref St1 state){}
-	void enumSmConv(EnumSmDef node, ref St1 state){}
-	void structConv(StructDef node, ref St1 state){}
 	void templateConv(TemplateDef node, ref St1){
 		node.unsupported;
 	}
-	void varConv(VarDef node, ref St1 state){}
-	void aliasConv(AliasDef node, ref St1 state){}
-	void unionConv(UnionDef node, ref St1 state){}
-	void utestConv(UTest node, ref St1 state){}
-
 	void cCompConv(CCNode node, ref St1){
 		node.unsupported;
 	}
 	void mixinInitConv(MixinInit node, ref St1){
 		node.unsupported;
 	}
+
+	void importConv(Import node, ref St1 state){
+		// TODO: merge imported stab
+	}
+
+	void fnConv(FnDef node, ref St1 state){
+		RFn fn = new RFn;
+		fn.ident = state.ctx.map!(i => i.toString).join(".")
+			.format!"%s.%s"(node.name);
+		fn.paramsN = node.params.params.map!(p => p.name).array;
+		fn.paramCount = fn.paramsN.length;
+		fn.pos = node.pos;
+		// TODO: figure out fn.body & fn.paramsT
+	}
+	void enumConstConv(EnumConstDef node, ref St1 state){}
+	void enumSmConv(EnumSmDef node, ref St1 state){}
+	void structConv(StructDef node, ref St1 state){}
+	void varConv(VarDef node, ref St1 state){}
+	void aliasConv(AliasDef node, ref St1 state){}
+	void unionConv(UnionDef node, ref St1 state){}
+	void utestConv(UTest node, ref St1 state){}
 }
 
 private void unsupported(ASTNode node){
-	assert(false, typeid(node).stringof ~ " not yet supported");
+	import std.conv : to;
+	assert(false, typeid(node).to!string ~ " not yet supported");
 }
