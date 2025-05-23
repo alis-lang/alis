2c847c5d7f05adbd1fddff0d11b0e131b0a6c35e
Nafees Hassan <nafeeshassan16@gmail.com>
2025-05-23 05:32:52 +0500

    eval overloads expecting type/value/symbol

--- source/alis/compiler/semantic/eval.d
+++ source/alis/compiler/semantic/eval.d
@@ -40,7 +40,6 @@ struct St{
 /// - `stab` - The root level Symbol Table
 /// - `ctx` - Context where the `expr` occurs
 /// Returns: AValCT, or SmErr[]
-pragma(inline, true)
 package SmErrsVal!AValCT eval(RExpr expr, STab stab, IdentU[] ctx){
 	St st;
 	st.stab = stab;
@@ -50,6 +49,39 @@ package SmErrsVal!AValCT eval(RExpr expr, STab stab, IdentU[] ctx){
 	if (st.errs.length)
 		return SmErrsVal!AValCT(st.errs);
 	return SmErrsVal!AValCT(st.res);
+}
+
+/// Evaluates an RExpr expecting a value. See `eval`
+/// Returns: AValCT with Type.Literal, or SmErr[]
+package SmErrsVal!AValCT eval4Val(RExpr expr, STab stab, IdentU[] ctx){
+	SmErrsVal!AValCT ret = eval(expr, stab, ctx);
+	if (ret.isErr)
+		return ret;
+	if (ret.val.type != AValCT.Type.Literal)
+		return SmErrsVal!AValCT([errExprValExpected(expr)]);
+	return ret;
+}
+
+/// Evaluates an RExpr expecting a type. See `eval`
+/// Returns: AValCT with Type.Type, or SmErr[]
+package SmErrsVal!AValCT eval4Type(RExpr expr, STab stab, IdentU[] ctx){
+	SmErrsVal!AValCT ret = eval(expr, stab, ctx);
+	if (ret.isErr)
+		return ret;
+	if (ret.val.type != AValCT.Type.Type)
+		return SmErrsVal!AValCT([errExprTypeExpected(expr)]);
+	return ret;
+}
+
+/// Evaluates an RExpr expecting a symbol. See `eval`
+/// Returns: AValCT with Type.Type, or SmErr[]
+package SmErrsVal!AValCT eval4Sym(RExpr expr, STab stab, IdentU[] ctx){
+	SmErrsVal!AValCT ret = eval(expr, stab, ctx);
+	if (ret.isErr)
+		return ret;
+	if (ret.val.type != AValCT.Type.Symbol)
+		return SmErrsVal!AValCT([errExprSymExpected(expr)]);
+	return ret;
 }
 
 /// Evaluates an Expression
@@ -66,4 +98,40 @@ package SmErrsVal!AValCT eval(Expression expr, STab stab, IdentU[] ctx,
 	if (resolved.isErr)
 		return SmErrsVal!AValCT(resolved.err);
 	return eval(resolved.val, stab, ctx);
+}
+
+/// Evaluates an Expression, expecting a value. See `eval`
+/// Returns: AValCT with Type.Literal, or SmErr[]
+package SmErrsVal!AValCT eval4Val(Expression expr, STab stab, IdentU[] ctx,
+		AValCT[] params = null){
+	SmErrsVal!AValCT ret = eval(expr, stab, ctx, params);
+	if (ret.isErr)
+		return ret;
+	if (ret.val.type != AValCT.Type.Literal)
+		return SmErrsVal!AValCT([errExprValExpected(expr)]);
+	return ret;
+}
+
+/// Evaluates an Expression, expecting a type. See `eval`
+/// Returns: AValCT with Type.Literal, or SmErr[]
+package SmErrsVal!AValCT eval4Type(Expression expr, STab stab, IdentU[] ctx,
+		AValCT[] params = null){
+	SmErrsVal!AValCT ret = eval(expr, stab, ctx, params);
+	if (ret.isErr)
+		return ret;
+	if (ret.val.type != AValCT.Type.Type)
+		return SmErrsVal!AValCT([errExprTypeExpected(expr)]);
+	return ret;
+}
+
+/// Evaluates an Expression, expecting a symbol. See `eval`
+/// Returns: AValCT with Type.Literal, or SmErr[]
+package SmErrsVal!AValCT eval4Sym(Expression expr, STab stab, IdentU[] ctx,
+		AValCT[] params = null){
+	SmErrsVal!AValCT ret = eval(expr, stab, ctx, params);
+	if (ret.isErr)
+		return ret;
+	if (ret.val.type != AValCT.Type.Symbol)
+		return SmErrsVal!AValCT([errExprSymExpected(expr)]);
+	return ret;
 }
