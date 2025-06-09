c9ae5bd5f20f5475e7dc5b91dbdd6852eab25f4f
Nafees Hassan <nafeeshassan16@gmail.com>
2025-06-09 16:58:01 +0500

    test input

--- source/alis/compiler/semantic/main.d
+++ source/alis/compiler/semantic/main.d
@@ -23,8 +23,9 @@ import alis.common,
 import core.stdc.stdlib;
 
 void main(){
-	string source;
-	while (!stdin.eof) source ~= stdin.readln;
+	import std.file;
+	string source = cast(string)File("in.alis").byLine.fold!((a, e) => a ~= e);
+	//while (!stdin.eof) source ~= stdin.readln;
 	StopWatch sw = StopWatch(AutoStart.yes);
 	CmpErrVal!Module node = source.tokenize.parse("alis-main");
 	sw.stop;
