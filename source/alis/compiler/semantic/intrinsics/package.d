0b4fa9beb0884be6c0b340174e8caee8fb25ee76
Nafees Hassan <nafeeshassan16@gmail.com>
2025-07-09 04:34:45 +0500

    export intrinsics via package

--- source/alis/compiler/semantic/intrinsics/package.d
+++ source/alis/compiler/semantic/intrinsics/package.d
@@ -2,3 +2,9 @@
 semantic.intrinsics package
 +/
 module alis.compiler.semantic.intrinsics;
+
+import alis.compiler.semantic.intrinsics.intrns;
+static import cmn = alis.compiler.semantic.intrinsics.common;
+
+public alias callabilityOf = cmn.callabilityOf!CallabilityCheckers;
+public alias resolve = cmn.resolve!ExprTranslators;
