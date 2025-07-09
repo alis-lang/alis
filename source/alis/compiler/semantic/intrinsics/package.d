a1fcbf090665ab2b91922afd43b2af00e438fd7b
Nafees Hassan <nafeeshassan16@gmail.com>
2025-07-09 05:07:14 +0500

    revert name, callabilityOfIntrN -> callabilityOf

--- source/alis/compiler/semantic/intrinsics/package.d
+++ source/alis/compiler/semantic/intrinsics/package.d
@@ -6,5 +6,5 @@ module alis.compiler.semantic.intrinsics;
 import alis.compiler.semantic.intrinsics.intrns;
 static import cmn = alis.compiler.semantic.intrinsics.common;
 
-public alias callabilityOfIntrN = cmn.callabilityOf!CallabilityCheckers;
+public alias callabilityOf = cmn.callabilityOf!CallabilityCheckers;
 public alias resolveIntrN = cmn.resolve!ExprTranslators;
