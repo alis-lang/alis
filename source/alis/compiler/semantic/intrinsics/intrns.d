70c7f50f0970923756d798b592e0b0887b56342e
Nafees Hassan <nafeeshassan16@gmail.com>
2025-07-16 13:18:23 +0500

    implemented a bunch of intrinsics

--- source/alis/compiler/semantic/intrinsics/intrns.d
+++ source/alis/compiler/semantic/intrinsics/intrns.d
@@ -9,16 +9,19 @@ import alis.common,
 			 alis.compiler.semantic.intrinsics.common,
 			 alis.compiler.semantic.error,
 			 alis.compiler.semantic.eval,
+			 alis.compiler.semantic.types,
 			 alis.compiler.ast,
 			 alis.compiler.ast.rst;
+
+import std.format;
 
 package alias CallabilityCheckers = CallabilityCheckersOf!(mixin(__MODULE__));
 package alias ExprTranslators = ExprTranslatorsOf!(mixin(__MODULE__));
 
-@Intr("ctWrite"){
+@Intr(IntrN.CTWrite){
 	@CallabilityChecker
-	bool ctWriteCanCall(AValCT[]) pure {
-		return true;
+	bool ctWriteCanCall(AValCT[] params) pure {
+		return params.length == 0;
 	}
 	@ExprTranslator
 	SmErrsVal!RExpr ctWriteTranslate(string, Location, STab,
@@ -26,5 +29,118 @@ package alias ExprTranslators = ExprTranslatorsOf!(mixin(__MODULE__));
 		import std.stdio : writefln;
 		writefln!"CTWRITE: %(%s%)"(params);
 		return SmErrsVal!RExpr(RNoOpExpr.instance);
+	}
+}
+
+@Intr(IntrN.Type){
+	@CallabilityChecker
+	bool typeCanCall(AValCT[]) pure {
+		return false;
+	}
+	@ExprTranslator
+	SmErrsVal!RExpr typeTranslate(string, Location pos, STab,
+			IdentU[], void[0][ASymbol*], RFn[string], AValCT[]){
+		return SmErrsVal!RExpr([errIntrUnk(pos, IntrN.Type)]);
+	}
+}
+
+@Intr(IntrN.NoInit){
+	@CallabilityChecker
+	bool noinitCanCall(AValCT[] params) pure {
+		return params.length == 0;
+	}
+	@ExprTranslator
+	SmErrsVal!RExpr noinitTranslate(string, Location pos, STab,
+			IdentU[], void[0][ASymbol*], RFn[string], AValCT[]){
+		RAValCTExpr r = new RAValCTExpr(ADataType.ofNoInit.AValCT);
+		r.pos = pos;
+		return SmErrsVal!RExpr(r);
+	}
+}
+
+@Intr(IntrN.NoInitVal){
+	@CallabilityChecker
+	bool noinitvalCanCall(AValCT[] params){
+		return params.length == 0;
+	}
+	@ExprTranslator
+	SmErrsVal!RExpr noinitvalTranslate(string, Location pos, STab,
+			IdentU[], void[0][ASymbol*], RFn[string], AValCT[]){
+		RAValCTExpr r = new RAValCTExpr(
+				AValCT(ADataType.ofNoInit, cast(ubyte[])null));
+		r.pos = pos;
+		return SmErrsVal!RExpr(r);
+	}
+}
+
+@CallabilityChecker
+@Intr(IntrN.Int)
+@Intr(IntrN.UInt)
+@Intr(IntrN.Float)
+@Intr(IntrN.Char)
+bool bitXCanCall(AValCT[] params){
+	if (params.length > 1)
+		return false;
+	AValCT p = params[0];
+	if (!p.asType.canCastTo(ADataType.ofInt))
+		return false;
+	int x = p.to(ADataType.ofInt).val.dataL.as!int; // TODO: handle `to` properly
+	if (x <= 0)
+		return false;
+	switch (x){
+		case 8, 16, 32:
+			static if (size_t.sizeof * 8 == 64){
+				case 64:
+					return true;
+			}
+		return true;
+		default:
+			return false;
+	}
+}
+
+@ExprTranslator{
+	@Intr(IntrN.Int)
+	SmErrsVal!RExpr intTranslate(string, Location pos, STab,
+			IdentU[], void[0][ASymbol*], RFn[string], AValCT[] params){
+		ubyte x = size_t.sizeof * 8;
+		if (params.length == 0)
+			x = cast(ubyte)params[0].to(ADataType.ofInt).val.dataL.as!int; // HACK
+		RAValCTExpr r = new RAValCTExpr(ADataType.ofInt(x).AValCT);
+		r.pos = pos;
+		return SmErrsVal!RExpr(r);
+	}
+
+	@Intr(IntrN.UInt)
+	SmErrsVal!RExpr uintTranslate(string, Location pos, STab,
+			IdentU[], void[0][ASymbol*], RFn[string], AValCT[] params){
+		ubyte x = size_t.sizeof * 8;
+		if (params.length == 0)
+			x = cast(ubyte)params[0].to(ADataType.ofInt).val.dataL.as!int; // HACK
+		RAValCTExpr r = new RAValCTExpr(ADataType.ofUInt(x).AValCT);
+		r.pos = pos;
+		return SmErrsVal!RExpr(r);
+	}
+
+	@Intr(IntrN.Float)
+	SmErrsVal!RExpr floatTranslate(string, Location pos, STab,
+			IdentU[], void[0][ASymbol*], RFn[string], AValCT[] params){
+		ubyte x = size_t.sizeof * 8;
+		if (params.length == 0)
+			x = cast(ubyte)params[0].to(ADataType.ofInt).val.dataL.as!int; // HACK
+		RAValCTExpr r = new RAValCTExpr(ADataType.ofFloat(x).AValCT);
+		r.pos = pos;
+		return SmErrsVal!RExpr(r);
+	}
+
+	@Intr(IntrN.Char)
+	SmErrsVal!RExpr charTranslate(string, Location pos, STab,
+			IdentU[], void[0][ASymbol*], RFn[string], AValCT[] params){
+		ubyte x = size_t.sizeof * 8;
+		if (params.length == 0)
+			x = cast(ubyte)params[0].to(ADataType.ofInt).val.dataL.as!int; // HACK
+		RAValCTExpr r = new RAValCTExpr(ADataType.ofChar(x).AValCT);
+		r.pos = pos;
+		return SmErrsVal!RExpr(r);
 	}
 }
